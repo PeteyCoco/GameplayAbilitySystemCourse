d3e541e876a279a0c746bda2e06b1d1665f956c7
PeteyCoco <peter.carsten.collins@gmail.com>
2024-09-26 14:07:42 -0400

    Added PP_Highlight material to show on enemy mouse over.

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -1,6 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Peter Carsten Collins (2024)
 
 #pragma once
 
 #include "CoreMinimal.h"
 
+
+#define CUSTOM_DEPTH_RED 250
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,10 +16,9 @@ class AURA_API AAuraEnemy : public AAuraCharacter, public IEnemyInterface
 	GENERATED_BODY()
 	
 public:
+	AAuraEnemy();
+
 	// IEnemyInterface overrides
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
 };
