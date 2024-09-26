56328ac881a2b3e88c4f288878254d26a2751a4e
PeteyCoco <peter.carsten.collins@gmail.com>
2024-09-26 15:00:27 -0400

    Created Player State for player GAS setup.

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+// Copyright Peter Carsten Collins (2024)
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; // Server will try to keep the player state up to date at 100Hz
+
+
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,20 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * The player character player state
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	
+};
