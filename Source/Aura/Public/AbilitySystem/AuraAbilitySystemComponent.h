e3f70afe9b4187947d344ca46a135ad2c72bb7db
PeteyCoco <peter.carsten.collins@gmail.com>
2024-09-26 15:06:35 -0400

    Created AbilitySystemComponent and AttributeSet for the project.

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * Aura base ability system component
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
