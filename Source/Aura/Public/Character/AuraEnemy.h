8155aef04344abed1189e7ab81c178dd2df77f38
PeteyCoco <peter.carsten.collins@gmail.com>
2024-09-26 13:20:39 -0400

    Created IEnemyInterface and added it to the AAuraEnemy class.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,14 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacter.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
- * 
+ * Character class for enemy units
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacter
+class AURA_API AAuraEnemy : public AAuraCharacter, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	// IEnemyInterface overrides
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+
 };
---
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -0,0 +1,26 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EnemyInterface.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * Interface for interacting with enemy characters
+ */
+class AURA_API IEnemyInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Highlight the actor in the scene
+	virtual void HighlightActor() = 0;
+	virtual void UnHighlightActor() = 0;
+};
