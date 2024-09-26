2ed9a9b895e0c1fc8af49c263b02e972553f0c75
PeteyCoco <peter.carsten.collins@gmail.com>
2024-09-26 11:46:01 -0400

    Created IMC and IA_Move. Created C++ player controller.

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
