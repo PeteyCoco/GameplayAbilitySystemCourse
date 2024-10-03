d90d52f323da1abcb74d001f2df89412d857291a
PeteyCoco <peter.carsten.collins@gmail.com>
2024-10-03 14:40:22 -0400

    Created AttributeInfo Data Asset and filled in values.

---
+++ Source/Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,56 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "AttributeInfo.generated.h"
+
+
+
+/*
+* Information struct that is passed up to widgets
+*/
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	// Identifying Gameplay Tag for this struct
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	// The attribute's name
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName = FText();
+
+	// A description for the attribute
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	// The attribute's value
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+/**
+ * Data asset for attribute information
+ */
+UCLASS()
+class AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraAttributeInfo> AttributeInformation;
+
+	/* 
+	* Get the attribute info for the given gameplay tag.
+	* Will print a debug message if tag not found and bLogNotFound = true
+	*/
+	UFUNCTION()
+	FAuraAttributeInfo FindAttributeInforForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+	
+};
