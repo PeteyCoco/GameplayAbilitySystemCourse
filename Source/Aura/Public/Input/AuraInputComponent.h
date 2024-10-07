a8b3a22fc9d4f3434adc88a5d3ec0a83396b8742
PeteyCoco <peter.carsten.collins@gmail.com>
2024-10-07 12:51:38 -0400

    Created AuraInputComponent, which allows us to bind input actions from a config file to callbacks that take in gameplay tags.

---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,49 @@
+// Copyright Peter Carsten Collins (2024)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputConfig.h"
+#include "AuraInputComponent.generated.h"
+
+/**
+ * An enhanced input component that allows us to pass gameplay tags into the input action callbacks
+ */
+UCLASS()
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+public:
+	// Binds ability actions using our input config
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc);
+	
+};
+
+template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+inline void UAuraInputComponent::BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+
+	for (const FAuraInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		// Bind the same actions to all input actions
+		// We use the gameplay tags to filter them later
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag);// InputTag is passed into HeldFunc
+			}
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag);
+			}
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag); 
+			}
+		}
+	}
+}
