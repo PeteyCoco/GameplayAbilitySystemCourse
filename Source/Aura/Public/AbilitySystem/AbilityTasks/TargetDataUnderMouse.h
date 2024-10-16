e0c9cf6b334e672d7473626c684b1532de1196fb
PeteyCoco <peter.carsten.collins@gmail.com>
2024-10-16 09:59:20 -0400

    Began implementation for SendMouseCursorData.

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -6,7 +6,7 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
 
 /**
  * 
@@ -27,4 +27,6 @@ private:
 	// Begin UAbilityTask interface
 	virtual void Activate() override;
 	// End UAbilityTask interface
+
+	void SendMouseCursorData();
 };
